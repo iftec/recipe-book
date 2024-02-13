b58affa78c2c15773bb07ce38e4aeb65742b1d9e
iftec <iftec@users.noreply.github.com>
2024-02-13 16:46:06 +0000

    Added remove from favorites and notification function

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -49,3 +49,47 @@ function printPageArea(recipeID) {
     window.print();
     document.body.innerHTML = originalContent;
 }
+
+async function removeFromFavorites(recipeId) {
+   
+    const confirmation = confirm('Are you sure you want to remove this recipe from favorites?');
+
+    if (confirmation) {
+        try {
+            const response = await fetch('/remove_from_favorites/' + recipeId, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ recipe_id: recipeId })
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+                var favoriteElement = document.getElementById('favorite-' + recipeId);
+                if (favoriteElement) {
+                    favoriteElement.remove();
+                }
+                
+                // Refresh the page after successful removal
+                location.reload();
+            } else {
+                showNotification('Failed to remove recipe from favorites.', false);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            showNotification('An error occurred while removing recipe from favorites.', false);
+        }
+    }
+
+    isRemoving = false; // Reset the flag after removal is complete
+}
+
+function showNotification(message, isSuccess) {
+    if (isSuccess) {
+        alert('Success: ' + message);
+    } else {
+        alert('Sorry: ' + message);
+    }
+}
